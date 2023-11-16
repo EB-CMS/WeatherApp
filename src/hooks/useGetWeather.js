be04ed308947787881039af0d3527f3257a906d4
EB-CMS <cmsebije@gmail.com>
2023-11-16 22:03:14 +0000

    cleaned up unused imports and comments

--- src/hooks/useGetWeather.js
+++ src/hooks/useGetWeather.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as Location from "expo-location";
 import { WEATHER_API_KEY } from "@env";
-import { GEOCAGE_API_KEY } from "@env";
 
 export const useGetWeather = () => {
   const [loading, setLoading] = useState(true);
@@ -9,9 +8,7 @@ export const useGetWeather = () => {
   const [weather, setWeather] = useState([]);
   const [lat, setLat] = useState(null);
   const [lon, setLon] = useState(null);
-  const [city, setCity] = useState(null);
   const [location, setLocation] = useState([]);
-  const [data, setData] = useState([]);
 
   //feching data (location datat) using the async function
   const getLoaction = async () => {
@@ -25,20 +22,6 @@ export const useGetWeather = () => {
     setLon(location.coords.longitude);
     setLocation(location);
   };
-
-  //fetching data from the GeoCageAPI
-  // const reverseGeoCode = async () => {
-  //   try {
-  //     const result = await fetch(
-  //       `https://api.opencagedata.com/geocode/v1/json?q=${lat},${lon}&key=${GEOCAGE_API_KEY}`
-  //     );
-  //     const data = await result.json();
-  //     setData(data);
-  //     setCity(data.results[0].components.city);
-  //   } catch (e) {
-  //     setError("couldn't reverse the geolocation");
-  //   }
-  // };
 
   //fetching data from the OpenWeatherMap API
   const fetchWeatherData = async () => {
@@ -59,10 +42,6 @@ export const useGetWeather = () => {
     getLoaction();
     fetchWeatherData();
   }, [lat, lon]);
-
-  // useEffect(() => {
-  //   reverseGeoCode();
-  // }, [lat, lon]); // this means that the effect only rerender when the lat and/or lon change/s
 
   return [loading, error, weather];
 };

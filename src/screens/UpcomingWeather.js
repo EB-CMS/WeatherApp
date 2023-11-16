a927dc3b4c8982c5dece5653d67853519e242bf3
EB-CMS <cmsebije@gmail.com>
2023-11-16 22:05:36 +0000

    cleaned up unused comments

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -8,47 +8,6 @@ import {
 } from "react-native";
 import ListItem from "../components/ListItem";
 
-//Testing Data
-/*const DATA = [
-  {
-    dt_txt: "2022-08-28 15:00:00",
-    main: {
-      temp_min: 27.84,
-      temp_max: 32.45,
-    },
-    weather: [
-      {
-        main: "Rain",
-      },
-    ],
-  },
-
-  {
-    dt_txt: "2022-08-29 15:00:00",
-    main: {
-      temp_min: 17.55,
-      temp_max: 19.55,
-    },
-    weather: [
-      {
-        main: "Clear",
-      },
-    ],
-  },
-  {
-    dt_txt: "2022-08-30 15:00:00",
-    main: {
-      temp_min: 26.84,
-      temp_max: 29.45,
-    },
-    weather: [
-      {
-        main: "Cloud",
-      },
-    ],
-  },
-];*/
-
 const UpcomingWeather = ({ weatherData }) => {
   //function
   const renderItem = ({ item }) => {
